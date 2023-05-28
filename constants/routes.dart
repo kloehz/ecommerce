ce1c763a59347f4aeeb5005e51cd499f620fd9f4
Ludmila Diaz <kludmiladiaz@gmail.com>
2023-05-28 14:07:39 -0300

    [ADD]Folders structure

---
+++ constants/routes.dart
@@ -0,0 +1,10 @@
+// import 'package:go_router/go_router.dart';
+
+// final _router = GoRouter(
+//   routes: [
+//     GoRoute(
+//       path: '/',
+//       builder: (context, state) => LoginScreen(),
+//     ),
+//   ],
+// );
---
+++ constants/urls.dart
@@ -0,0 +1,7 @@
+const baseURL = 'https://api.mercadolibre.com/sites/MLA';
+
+class Endpoints {
+  Endpoints._();
+
+  static const search = '/search?q=';
+}
