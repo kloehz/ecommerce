a08739c1926974ad4ece0abd3ba06834b8221559
Ludmila Diaz <kludmiladiaz@gmail.com>
2023-08-05 21:44:06 -0300

    [ADD]Parallex effect

--- lib/modules/home/presenter/view/widgets/category_widget.dart
+++ lib/modules/home/presenter/view/widgets/category_widget.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce/constants/colors.dart';
 import 'package:ecommerce/modules/home/data/models/get_categories_response/get_categories_response.dart';
+import 'package:ecommerce/modules/home/presenter/view/widgets/parallax_effect.dart';
 import 'package:ecommerce/utils/shared_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -15,53 +16,58 @@ class CategoryWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final width = context.width;
     return Container(
-      width: width * 0.46,
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-          color: Colors.white, borderRadius: BorderRadius.circular(20)),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                  decoration: BoxDecoration(
-                      color: Colors.lightBlue[100],
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 4),
-                      child: Text(
-                        '30%',
-                        style: TextStyle(fontWeight: FontWeight.w800),
-                      ))),
-              const Icon(
-                Icons.favorite,
-                color: Colors.grey,
-              )
-            ],
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(20),
-              child: Image.network(
-                category.image,
-                fit: BoxFit.contain,
-              ),
-            ),
-          ),
-          Text(
-            category.title,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: const TextStyle(
-                color: AppColors.primaryColor,
-                fontWeight: FontWeight.w500,
-                fontSize: 18),
-          ),
-          const ReviewStars()
-        ],
-      ),
-    );
+        width: width * 0.46,
+        // padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(20)),
+        child: ParallaxEffectWidget(
+          country: 'Pergamino',
+          title: category.title,
+          imageUrl: category.image,
+        )
+        // child: Column(
+        //   children: [
+        //     Row(
+        //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //       children: [
+        //         Container(
+        //             decoration: BoxDecoration(
+        //                 color: Colors.lightBlue[100],
+        //                 borderRadius: BorderRadius.circular(12)),
+        //             child: const Padding(
+        //                 padding: EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+        //                 child: Text(
+        //                   '30%',
+        //                   style: TextStyle(fontWeight: FontWeight.w800),
+        //                 ))),
+        //         const Icon(
+        //           Icons.favorite,
+        //           color: Colors.grey,
+        //         )
+        //       ],
+        //     ),
+        //     Expanded(
+        //       child: Container(
+        //         padding: const EdgeInsets.all(20),
+        //         child: Image.network(
+        //           category.image,
+        //           fit: BoxFit.contain,
+        //         ),
+        //       ),
+        //     ),
+        //     Text(
+        //       category.title,
+        //       maxLines: 1,
+        //       overflow: TextOverflow.ellipsis,
+        //       style: const TextStyle(
+        //           color: AppColors.primaryColor,
+        //           fontWeight: FontWeight.w500,
+        //           fontSize: 18),
+        //     ),
+        //     const ReviewStars()
+        //   ],
+        // ),
+        );
   }
 }
 
